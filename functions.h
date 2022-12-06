f7fa466a181064ecd8f7205d016fdbab0d9a05dc
Sarkon Kyo <ilmar.keio@gmail.com>
2022-12-06 10:32:53 +0100

    KylskåpsLoopar

---
+++ functions.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector> 
+#include <iomanip>
+#include <cmath>
+#include <stdlib.h> //För att använda Rand
+using namespace std; 
---
+++ kylskåpsloopar.cpp
@@ -0,0 +1,44 @@
+#include "functions.h"
+
+void kylskapMedWhileTrue(){
+    cout << "Kylskåp med while true" << endl;
+    while(true){
+        double num; 
+        double temp = -273;
+        cout << "Enter a tmep: "; 
+        cin >> num; 
+        if (num >= temp){
+            if (num <=2)
+                cout << "För Kallt!" << endl; 
+            else if (num >= 8) 
+                cout << "För varmt!" << endl;
+            else if (num > 2 && num < 8)
+                cout << "Lagom" << endl; 
+         } 
+         if (num < temp)
+                break;
+
+    }
+    cout << "Lämnar kylskåsloop." << endl;
+}
+
+void kylskapMedWhileVillkor(){
+    cout << "Kylskåp med While villkor" << endl;
+    double temp = 0; 
+    while (temp>-273){
+        cout << "Enter temp: "; 
+        cin >> temp;
+        if (temp <= 2 && temp>-273)
+        cout << "För kallt!" << endl; 
+        if (temp >= 8) 
+        cout << "För varmt!" << endl;
+    }
+    cout << "Lämnar loopen nu!" << endl; 
+ }
+void ingangTill_funkltion(){
+    kylskapMedWhileTrue();
+
+}
+int main(){
+kylskapMedWhileVillkor();
+}
